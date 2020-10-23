71742e5b33dce7fdf20ad5d3dba04a6b85f305e4
AleksandrDeveloper <aleks777.chernof7@mail.ru>
2020-10-23 17:13:35 +0300

    itin

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require('./hrader.php') ?>
+
+<form method="post" action="action.php">
+    <input type="text" name="name">
+    <button type="submit">dfff</button>
+</form>
+
+<form method="get" action="get.php">
+    <input type="text" name="name">
+    <button type="submit">dfff</button>
+</form>
+
+<?php include './footer.php' ?>
