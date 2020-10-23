f6856620843fc5793ade0a9cdb2363871123667e
AleksandrDeveloper <aleks777.chernof7@mail.ru>
2020-10-24 01:02:03 +0300

    f

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
-<?php require('./hrader.php') ?>
+<?php  
 
-<form method="post" action="action.php">
-    <input type="text" name="name">
-    <button type="submit">dfff</button>
-</form>
+    $db = mysqli_connect('127.0.0.1','root','root','my_db');
+    if(!$db) die (mysqli_connect_error());
 
-<form method="get" action="get.php">
-    <input type="text" name="name">
-    <button type="submit">dfff</button>
-</form>
+    mysqli_set_charset($db,'utf8') or die(mysqli_connect_error());
+    $insert = "SELECT name, text, data FROM user ORDER BY ID DESC ";
+    $data = mysqli_fetch_all(mysqli_query($db,$insert),MYSQLI_ASSOC);
 
-<?php include './footer.php' ?>
+    foreach ($data as $value) {
+        print_r("Nmae: {$value['name']} <br>");
+        print_r("Text: {$value['text']} <br>");
+        print_r("Data: {$value['data']} <br>");
+        echo('<hr>');
+    };
+?>
