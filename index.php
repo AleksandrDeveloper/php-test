9d5e8fd23243e1bc7c805fd4d029edcb2cd5b624
AleksandrDeveloper <aleks777.chernof7@mail.ru>
2020-10-24 01:02:19 +0300

    f

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php  
 
-    $db = mysqli_connect('127.0.0.1','root','root','my_db');
+    $db = mysqli_connect('','','','my_db');
     if(!$db) die (mysqli_connect_error());
 
     mysqli_set_charset($db,'utf8') or die(mysqli_connect_error());
